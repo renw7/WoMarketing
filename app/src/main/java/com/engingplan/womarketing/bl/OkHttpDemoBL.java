5d13d1295787d980d906ab63fad0e93aac895dea
renw <1234qwer>
2019-11-06 16:29:45 +0800

    修改

---
+++ app/src/main/java/com/engingplan/womarketing/bl/OkHttpDemoBL.java
@@ -0,0 +1,138 @@
+package com.engingplan.womarketing.bl;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.engingplan.womarketing.util.OkHttpClientUtils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *    author : Android
+ *    github : https://github.com/renw7/AndroidProject
+ *    time   : 2018/10/31
+ *    desc   : http请求demo bl层
+ */
+public class OkHttpDemoBL {
+
+    //云服务器地址
+    private String url = "http://119.29.106.248/tblcallrecord/page";
+
+
+
+
+    //本机测试地址
+//    private String url = "http://10.52.200.150/tbluserinfo/page";
+
+    /**
+     * 异步http调用
+     * @param param
+     */
+    public void getUserInfoAllAsyn(Map param, Context context){
+        OkHttpClientUtils.getInstance().doGetAsyn(url, param,new OkHttpClientUtils.NetWorkCallBack(){
+            @Override
+            public void onSuccess(String response) {
+                ArrayList<Map> list = json2List(response);
+
+
+                //下面通过异常方式返回给ui层
+                Intent intent = new Intent("abc");
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("list", list);
+                intent.putExtras(bundle);       //向广播接收器传递数据
+                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+            }
+
+            @Override
+            public void onFail(String response) {
+                System.out.println(response);
+            }
+        });
+
+    }
+
+
+    private ArrayList<Map> json2List(String result){
+        ArrayList<Map> recordList = new ArrayList<>();
+
+        try {
+            JSONObject root = new JSONObject(result);
+            String code = root.getString("code");
+
+            if ("200".equals(code)) {
+                JSONArray jsonArray = root.getJSONObject("data").getJSONArray("records");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject record = jsonArray.getJSONObject(i);
+                    Map map = new HashMap();
+                    map.put("createUser", record.getString("createUser"));
+                    map.put("recordId", record.getString("recordId"));
+                    map.put("createTime", record.getString("createTime"));
+                    map.put("updateUser", record.getString("updateUser"));
+                    map.put("updateTime", record.getString("updateTime"));
+                    map.put("status", record.getString("status"));
+                    map.put("isDeleted", record.getString("isDeleted"));
+                    map.put("serialNumber", record.getString("serialNumber"));
+                    map.put("taskId", record.getString("taskId"));
+                    map.put("startTime", record.getString("startTime"));
+                    map.put("endTime", record.getString("endTime"));
+                    map.put("resultCode", record.getString("resultCode"));
+                    map.put("productId", record.getString("productId"));
+                    map.put("callTimes", record.getString("callTimes"));
+                    map.put("remark", record.getString("remark"));
+                    map.put("staffId", record.getString("staffId"));
+                    map.put("resultDesc", record.getString("resultDesc"));
+                    map.put("taskName", record.getString("taskName"));
+                    recordList.add(map);
+                }
+            }
+            else{
+                throw new Exception("连接异常["+code+"]");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return recordList;
+        }
+    }
+    public void getUserInfoAllAsyn2(Map param, String url2, Context context){
+        OkHttpClientUtils.getInstance().doGetAsyn(url2, param,new OkHttpClientUtils.NetWorkCallBack(){
+            @Override
+            public void onSuccess(String response) {
+                ArrayList<Map> list = json2List(response);
+
+
+                //下面通过异常方式返回给ui层
+                Intent intent = new Intent("det");
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("list", list);
+                intent.putExtras(bundle);       //向广播接收器传递数据
+                LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+            }
+
+            @Override
+            public void onFail(String response) {
+                System.out.println(response);
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/engingplan/womarketing/ui/activity/CallDetailActivity.java
@@ -0,0 +1,111 @@
+package com.engingplan.womarketing.ui.activity;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.engingplan.womarketing.bl.OkHttpDemoBL;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CallDetailActivity extends Activity {
+    TextView detail_nun=null;
+    TextView task_name = null;
+    TextView detail_startime = null;
+    TextView detail_endtime = null;
+    TextView detail_result = null;
+    TextView remark =null;
+    String  taskId=null;
+    String  serialNumber=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_detail);
+       detail_nun = findViewById(R.id.detail_nun);
+       task_name = findViewById(R.id.detail_task);
+         detail_startime = findViewById(R.id.detail_startime);
+        detail_endtime = findViewById(R.id.detail_endtime);
+        detail_result = findViewById(R.id.detail_result);
+       remark = findViewById(R.id.detail_remark);
+
+        Bundle bundle = this.getIntent().getExtras();
+        String recordId=bundle.getString("recordId");
+
+//        detail_nun.setText(bundle.getString("serialNumber"));
+//        task_name.setText(bundle.getString("taskId"));
+//        detail_startime.setText(bundle.getString("startTime"));
+//        detail_endtime.setText(bundle.getString("endTime"));
+//        detail_result.setText(bundle.getString("resultCode"));
+//        remark.setText(bundle.getString("remark"));
+
+
+
+        //        返回键
+        Button btn1 = findViewById(R.id.detail_return);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        //跳转再次拨打界面，传递参数号码
+        Button btn2 = findViewById(R.id.detail_recall);
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(CallDetailActivity.this, DialActivity.class);
+
+                it.putExtra("serialNumber",serialNumber);
+                it.putExtra("taskId",taskId);
+                startActivity(it);
+            }
+
+          //  @Override
+           // public void onClick(View view) {
+              //  finish();
+            //}
+        });
+        //接收
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("det");
+        getApplicationContext().registerReceiver(mReceiver, intentFilter);
+
+        OkHttpDemoBL okHttpDemoBL = new OkHttpDemoBL();
+        //传递参数
+        Map param = new HashMap<>();
+//        param.put("username", "张三");
+        String url="http://119.29.106.248/tblcallrecord/info?recordId="+recordId;
+        System.out.println(url);
+        okHttpDemoBL.getUserInfoAllAsyn2(param, url,this.getApplicationContext());
+        }
+
+    private BroadcastReceiver mReceiver = new CallDetailActivity.DataReceiver();
+    class DataReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<Map<String, String>> list = (List)intent.getExtras().get("list");
+            System.out.println("size="+list.size());
+            detail_nun.setText(list.get(0).get("serialNumber"));
+            task_name.setText(list.get(0).get("taskName"));
+            detail_startime.setText(list.get(0).get("startTime"));
+            detail_endtime.setText(list.get(0).get("endTime"));
+            detail_result.setText(list.get(0).get("resultDesc"));
+            remark.setText(list.get(0).get("remark"));
+            serialNumber=list.get(0).get("serialNumber");
+            taskId=list.get(0).get("taskId");
+
+
+        }
+    }
+    }
+
