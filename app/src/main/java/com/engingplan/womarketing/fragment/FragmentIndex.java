6de80d4034cd8daeeb4ec97a2d989b0806fc1b08
Administrator <tang.727492333@qq.com>
2019-11-06 17:31:23 +0800

    1

--- app/src/main/java/com/engingplan/womarketing/fragment/FragmentIndex.java
+++ app/src/main/java/com/engingplan/womarketing/fragment/FragmentIndex.java
@@ -1,16 +1,21 @@
 package com.engingplan.womarketing.fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.engingplan.womarketing.bl.IndexBL;
 import com.engingplan.womarketing.ui.activity.R;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
@@ -22,15 +27,20 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 
 public class FragmentIndex extends Fragment {
-    SwipeRefreshLayout swipeRefresh;
 
-
+    private String ACTION_APP_BROADCAST = "com.engingplan.womarketing.fragment";
+    LocalBroadcastManager broadcastManager;
+    int a;
+    BarChart barChart = null;
+    BarChart barChart1 = null;
     public static FragmentIndex newInstance(String name) {
+        Bundle args = new Bundle();
+        args.putString("name", name);
         FragmentIndex fragment = new FragmentIndex();
+        fragment.setArguments(args);
         return fragment;
     }
 
@@ -38,62 +48,76 @@ public class FragmentIndex extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_fragmentindex, container, false);
+        //这里实例化广播管理者
+        broadcastManager = LocalBroadcastManager.getInstance(getContext());
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Bundle bundle = this.getArguments();
-        String staffName = null;
-        if (bundle != null){
-            staffName = bundle.getString("staffname");
-            TextView textView= view.findViewById(R.id.textView3);
-            textView.setText(staffName);
-        }
-
-        BarChart barChart = view.findViewById(R.id.ChartTest);//定义界面控件
-        BarChart barChart1 = view.findViewById(R.id.ChartTest1);//定义界面控件
+        //这里注册广播接收器
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_APP_BROADCAST);
+        broadcastManager.registerReceiver(mReceiver, intentFilter);
+        //实例化IndexBL调取方法
+        IndexBL indexBL = new IndexBL();
+        indexBL.setDataFirst(broadcastManager);
+        barChart = view.findViewById(R.id.ChartTest);//定义界面控件
+        barChart1 = view.findViewById(R.id.ChartTest1);//定义界面控件
         barChart = initBarChart(barChart);//调用方法初始化柱状图
         barChart1 = initBarChart(barChart1);//调用方法初始化柱状图
-        BarData barData = setbarData();//调用方法初始化数据
-        BarData barData1 = setbarData();//调用方法初始化数据
-        barChart.setData(barData);//将数据用到柱状图上显示
-        barChart1.setData(barData1);//将数据用到柱状图上显示
-        barChart.invalidate();//在柱状图填充数据以后进行刷新
-        barChart1.invalidate();//在柱状图填充数据以后进行刷新
 
 
-        swipeRefresh = view.findViewById(R.id.swipeRefresh);
-        swipeRefresh.setColorSchemeResources(R.color.colorPrimary, R.color.colorAccent, R.color.white, R.color.white);
-        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                swipeRefresh.setRefreshing(false);
-            }
-        });
+
+
+
+    }
+
+    /**
+     * 广播接收器
+     */
+    private BroadcastReceiver mReceiver = new MyReceiver2();
+
+    class MyReceiver2 extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            a =  intent.getIntExtra("num", 0);
+            BarData barData = setbarData();//调用方法初始化数据
+            BarData barData1 = setbarData();//调用方法初始化数据
+            barChart.setData(barData);//将数据用到柱状图上显示
+            barChart1.setData(barData1);//将数据用到柱状图上显示
+            barChart.invalidate();//在柱状图填充数据以后进行刷新
+            barChart1.invalidate();//在柱状图填充数据以后进行刷新
+            Log.i("MyReceiver", "啦啦啦啦啦啦啦啦绿绿绿绿..." + a);
+        }
     }
 
 
-    public BarData setbarData(){
+
+    public BarData setbarData() {
 
         List<BarEntry> entries = new ArrayList<>();//定义一个entries的集合用来存放数据
         //为容器添加数据，为200以内随机数
-        for(int i = 1 ;i < 4;i++){
-            entries.add(new BarEntry(i,new Random().nextInt(200)));
+        for (int i = 1; i < 4; i++) {
+            entries.add(new BarEntry(i,a));
         }
-        BarDataSet barDataSet = new BarDataSet(entries,null);//设置数据集
-        BarData  barData = new BarData(barDataSet);//把数据集放到barData对象里
-        barData.setBarWidth(0.6f);//每个柱子的宽度
-        barDataSet.setColors(new int[]{Color.rgb(255,117,6),
-                Color.rgb(255,149,63),
-                Color.rgb(255,179,18),});
+        BarDataSet barDataSet = new BarDataSet(entries, null);//设置数据集
+        BarData barData = new BarData(barDataSet);//把数据集放到barData对象里
+        barData.setBarWidth(0.7f);//每个柱子的宽度
+        barDataSet.setColors(new int[]{Color.rgb(255, 117, 6),
+                Color.rgb(255, 149, 63),
+                Color.rgb(255, 179, 18),});
+
         return barData;
 
 
     }
+
     //这个方法用来初始化柱状图
-    public  BarChart initBarChart(BarChart barChart){ // 传入barchart实例
+    public BarChart initBarChart(BarChart barChart) { // 传入barchart实例
+
         barChart.setDescription(null);//设置柱状图的描述
         barChart.setDrawBarShadow(false);//设置每个柱子的阴影不显示
         barChart.setDrawValueAboveBar(true);//设置每个柱子的数值显示
@@ -101,30 +125,33 @@ public class FragmentIndex extends Fragment {
         //barChart.getXAxis().setDrawLabels(false);//设置不显示横坐标
         barChart.getAxisRight().setDrawLabels(false);//设置不显示右侧纵坐标
         barChart.getAxisLeft().setDrawLabels(false);//设置不显示左侧纵坐标
+
         XAxis xAxis = barChart.getXAxis();//获取柱状图X轴
         YAxis yAxisLeft = barChart.getAxisLeft();//获取柱状图左侧Y轴
         YAxis yAxisRight = barChart.getAxisRight();//获取柱状图右侧Y轴
-        setAxis(xAxis,yAxisLeft);//调用方法设置柱状图的轴线
+
+        setAxis(xAxis, yAxisLeft);//调用方法设置柱状图的轴线
+
         return barChart;//这里返回初始化完成的柱状图实例
     }
 
     //这个方法用来设置柱状图的X,Y轴
-    public void setAxis(XAxis xAxis,YAxis leftYaxis){
+    public void setAxis(XAxis xAxis, YAxis leftYaxis) {
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//设置X轴在柱状图底部显示
         xAxis.setAxisLineWidth(1);//设置X轴宽度
         xAxis.setAxisMinimum(0);//设置X轴从0开始
         xAxis.setDrawAxisLine(true);//设置X轴显示轴线
         xAxis.setDrawGridLines(false);//设置X轴的表格线不显示
         xAxis.setEnabled(true);//设置X轴显示
-        xAxis.setSpaceMax(3);
+        xAxis.setSpaceMax(3);//设置柱间间距
 
         final List<String> xValue = new ArrayList<>();
+
         xValue.add("");//index = 0 的位置的数据在IndexAxisValueFormatter中时不显示的。
         xValue.add("已完成任务");
         xValue.add("未完成任务");
         xValue.add("有意向客户");
         xAxis.setValueFormatter(new IndexAxisValueFormatter(xValue));//设置x轴标签格式化器
-
 
 
         leftYaxis.setAxisMinimum(0);//设置左侧Y轴从0开始
