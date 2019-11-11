dedf1a25d60039adcdd324f0f87095c42a739584
Administrator <tang.727492333@qq.com>
2019-11-11 10:24:53 +0800

    2

--- app/src/main/java/com/engingplan/womarketing/bl/IndexBL.java
+++ app/src/main/java/com/engingplan/womarketing/bl/IndexBL.java
@@ -1,4 +1,5 @@
 package com.engingplan.womarketing.bl;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,7 +11,9 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -18,18 +21,24 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
-public class IndexBL  {
+public class IndexBL {
 
-     private String url = "http://119.29.106.248/tblcallrecord/statistics";
-     private  int todayFinishNum ;
-     private  int todayIntentNum ;
-     private  int todayIncompNum ;
-     private String ACTION_APP_BROADCAST = "com.engingplan.womarketing.fragment";
-     private int staffId ;
-     //接受传来的staffId
-     public IndexBL(int staffId){
-         this.staffId = staffId;
-     }
+    private String url = "http://119.29.106.248/tblcallrecord/statistics";
+    private int todayFinishNum;
+    private int todayIntentNum;
+    private int todayIncompNum;
+    private int weekFinishNum;
+    private int weekIncompNum;
+    private int weekIntentNum;
+    private String ACTION_APP_BROADCAST = "today";
+    private String ACTION_APP_BROADCAST2 = "week";
+    private int staffId;
+
+    //接受传来的staffId
+    public IndexBL(int staffId) {
+        this.staffId = staffId;
+    }
+
     /**
      * 这个方法用来从后台调取数据
      */
@@ -38,17 +47,20 @@ public class IndexBL  {
 
 
         String url1 = url + "?staffId=" + staffId;
+        String url2 = url + "/tblcallrecord/statisticsweek" + staffId;
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url1).get().build();
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
+        Request request1 = new Request.Builder().url(url1).get().build();
+        Request request2 = new Request.Builder().url(url2).get().build();
+        Call call1 = client.newCall(request1);
+        Call call2 = client.newCall(request2);
+        call1.enqueue(new Callback() {
             @Override
-            public void onFailure( Call call,  IOException e) {
+            public void onFailure(Call call, IOException e) {
                 System.out.println("调取失败");
             }
 
             @Override
-            public void onResponse( Call call,  Response response) throws IOException {
+            public void onResponse(Call call, Response response) throws IOException {
                 ResponseBody responseBody = response.body();
                 String responseString = responseBody.string();
                 Log.d("调取的数据是：", responseString);
@@ -62,7 +74,7 @@ public class IndexBL  {
                 JSONArray jsonArray = null;
                 try {
                     jsonArray = jsonObject.getJSONObject("data").getJSONArray("records");
-                    JSONObject o = (JSONObject)jsonArray.get(0);
+                    JSONObject o = (JSONObject) jsonArray.get(0);
                     todayFinishNum = (Integer) o.get("finishNum");
                     todayIntentNum = (Integer) o.get("intentNum");
                     todayIncompNum = (Integer) o.get("incompNum");
@@ -76,20 +88,56 @@ public class IndexBL  {
                 Intent intent = new Intent();
                 intent.setAction(ACTION_APP_BROADCAST);
                 intent.putExtra("finishNum", todayFinishNum);
-                intent.putExtra("intentNum",todayIntentNum);
-                intent.putExtra("incompNum",todayIncompNum);
+                intent.putExtra("intentNum", todayIntentNum);
+                intent.putExtra("incompNum", todayIncompNum);
+                intent.putExtra("type",1);
                 broadcastManager.sendBroadcast(intent);
             }
+
+        });
+        call2.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println("调取失败");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                ResponseBody responseBody = response.body();
+                String responseString = responseBody.string();
+                Log.d("调取的数据是：", responseString);
+
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(responseString);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = jsonObject.getJSONObject("data").getJSONArray("records");
+                    JSONObject o = (JSONObject) jsonArray.get(0);
+                    weekFinishNum = (Integer) o.get("finishNum");
+                    weekIntentNum = (Integer) o.get("intentNum");
+                    weekIncompNum = (Integer) o.get("incompNum");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+                Intent intent = new Intent();
+                intent.setAction(ACTION_APP_BROADCAST);
+                intent.putExtra("finishNum", weekFinishNum);
+                intent.putExtra("intentNum", weekIntentNum);
+                intent.putExtra("incompNum", weekIncompNum);
+                intent.putExtra("type",2);
+                broadcastManager.sendBroadcast(intent);
+            }
+
+
         });
 
 
-
-
-
-
     }
-
-
-
-
 }
