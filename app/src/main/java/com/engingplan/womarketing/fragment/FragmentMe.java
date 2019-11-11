00322f430c816e28feab58ee559a0933910f34d9
renw <1234qwer>
2019-11-11 13:54:04 +0800

    修改

--- app/src/main/java/com/engingplan/womarketing/fragment/FragmentMe.java
+++ app/src/main/java/com/engingplan/womarketing/fragment/FragmentMe.java
@@ -112,12 +112,7 @@ public class FragmentMe extends Fragment {
                 switch (flag){
                     //开始下载
                     case ConstantsUtil.flag_start:
-                        flag = ConstantsUtil.flag_doing;
-                        Toast.makeText(mContext, "开始下载！", Toast.LENGTH_SHORT).show();
-                        Map m = new HashMap();
-                        m.put("staffId", staffId);
-                        UpdatePwdBL updatePwdBL = new UpdatePwdBL();
-                        updatePwdBL.workReport(m, getContext());
+                        downloadDialog();
                         break;
                     //下载中
                     case ConstantsUtil.flag_doing:
@@ -287,6 +282,40 @@ public class FragmentMe extends Fragment {
 
     }
 
+
+
+    //下载对话框
+    private void downloadDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        final View v = inflater.inflate(R.layout.activity_down_dialog, null);
+        Button btnSure = v.findViewById(R.id.dialog_btn_sure);
+        Button btnCancel = v.findViewById(R.id.dialog_btn_cancel);
+        final Dialog dialog = builder.create();
+        dialog.show();
+        dialog.getWindow().setContentView(v);
+        dialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+
+        btnSure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                flag = ConstantsUtil.flag_doing;
+                Toast.makeText(mContext, "开始下载！", Toast.LENGTH_SHORT).show();
+                Map m = new HashMap();
+                m.put("staffId", staffId);
+                UpdatePwdBL updatePwdBL = new UpdatePwdBL();
+                updatePwdBL.workReport(m, getContext());
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+    }
 
 
     //从这里结束
